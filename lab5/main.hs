8befe6144b48be4568bf6892d46eb34e3bea6e10
rob3rtu <robertudrea2002@yahoo.ro>
2022-11-01 11:26:56 +0200

    gata lab5

---
+++ lab5/main.hs
@@ -0,0 +1,46 @@
+-- 1
+sumOdd :: [Int] -> Int
+sumOdd l = foldl (+) 0 (map (^ 2) (filter odd l))
+
+-- 2
+allTrue :: [Bool] -> Bool
+allTrue = foldr (&&) True
+
+-- 3
+allVerifies :: (Int -> Bool) -> [Int] -> Bool
+allVerifies f l
+  | length (filter f l) == length l = True
+  | otherwise = False
+
+-- 4
+anyVerifies :: (Int -> Bool) -> [Int] -> Bool
+anyVerifies f l
+  | length (filter f l) > 0 = True
+  | otherwise = False
+
+-- 5
+mapFoldr :: (a -> b) -> [a] -> [b]
+mapFoldr _ [] = []
+mapFoldr f l = foldr (\x xs -> f x : xs) [] l
+
+filterFoldr :: (a -> Bool) -> [a] -> [a]
+filterFoldr _ [] = []
+filterFoldr f l = foldr (\x xs -> if f x then x : xs else xs) [] l
+
+-- 6
+listToInt :: [Integer] -> Integer
+listToInt l = foldl (\a b -> a * 10 + b) 0 l
+
+-- 7
+-- a
+rmChar :: Char -> String -> String
+rmChar ch = filter (/= ch)
+
+-- b
+rmCharsRec :: String -> String -> String
+rmCharsRec [] l = l
+rmCharsRec (h : t) s2 = rmCharsRec t (rmChar h s2)
+
+-- c
+rmCharsFold :: String -> String -> String
+rmCharsFold s1 = foldr (\x xs -> if x `elem` s1 then x : xs else xs) []
